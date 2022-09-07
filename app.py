1d4e0c94b093e99104684ed28e033c4f3ca43638
jiwanlk <jivanparajuli@outlook.com>
2022-09-07 11:47:23 +0545

    add redis

--- app.py
+++ app.py
@@ -1,11 +1,20 @@
-from flask import Flask, render_template
+import os
+from flask import Flask
+import redis
+
+redis_url = os.getenv("REDISTOGO_URL", "redis://redis:6379")
+
+rdb = redis.from_url(redis_url)
+if not rdb.exists("counter"):
+    rdb.set("counter", 0)
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def index():
-    return "<h1>Hello, World!</h1>"
+    rdb.incr("counter", 1)
+    return f"<h1>This page has been visited {rdb.get('counter').decode()} times!</h1>"
 
 
 if __name__ == "__main__":
