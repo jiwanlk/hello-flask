98e8a4ad6e46e88c98625d827d97fa8e6080758d
jiwanlk <jivanparajuli@outlook.com>
2022-09-07 17:12:31 +0545

    task queue implementation

--- app.py
+++ app.py
@@ -1,17 +1,51 @@
-from flask import Flask, request
+import datetime
+import json
+from flask import Flask, request, make_response, render_template, redirect
 from redis import Redis
-from rq import Queue
+from rq import Queue, job
 from worker import count_words_at_url
 
 app = Flask(__name__)
-queue = Queue(connection=Redis(host="redis"))
+r = Redis(host="redis")
+queue = Queue(connection=r)
 
 
 @app.route("/")
 def index():
+    urls_raw = request.cookies.get("urls_in_queue", "[]")
+    urls = json.loads(urls_raw)
+    return render_template("index.html", urls=urls)
+
+
+@app.route("/add_to_queue")
+def add_to_queue():
     if "url" in request.args:
-        queue.enqueue(count_words_at_url, request.args.get("url"))
-    return "Counting going on..."
+        url = request.args.get("url")
+        if not url[:8].startswith(("https://", "http://")):
+            url = "http://" + url
+        urls_raw = request.cookies.get("urls_in_queue", "[]")
+        urls = json.loads(urls_raw)
+        job = queue.enqueue(count_words_at_url, url, result_ttl=60)
+        urls = [*urls, {url: job.id}]
+        url_json = json.dumps(urls)
+        res = make_response(redirect("/"))
+        res.set_cookie(
+            "urls_in_queue",
+            url_json,
+            expires=datetime.datetime.now() + datetime.timedelta(minutes=1),
+        )
+        return res
+
+
+@app.route("/results/<job_key>", methods=["GET"])
+def get_results(job_key):
+    if not job.Job.exists(job_key, connection=r):
+        return "No job", 400
+    j = job.Job.fetch(job_key, connection=r)
+    if j.is_finished:
+        return str(j.result), 200
+    else:
+        return "No result", 202
 
 
 if __name__ == "__main__":
--- worker.py
+++ worker.py
@@ -1,7 +1,9 @@
+import time
 import requests
 
 
 def count_words_at_url(url):
     """counts word in url website"""
+    time.sleep(15)
     resp = requests.get(url)
     return len(resp.text.split())
