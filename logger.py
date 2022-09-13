00bcb02d28d3bee68135b4ea87d7d9d6b266e9e8
jiwanlk <jivanparajuli@outlook.com>
2022-09-13 11:28:49 +0545

    fix loguru config

--- logger.py
+++ logger.py
@@ -9,9 +9,18 @@ IS_PROD = os.getenv("ENVIRO") == "prod"
 new_level = logger.level("WORKER", no=10, color="<RED>", icon="👷")
 
 logger.remove()
+
 if IS_PROD:
-    logger.add("test.log", retention="1 month", level="INFO")
+    # For saving logger to out.log in server
+    logger.add("./logs/out.log", backtrace=True, diagnose=True)
 else:
-    logger.add(sys.stderr, format="{time} - {level} - {message}", level="DEBUG")
+    logger.add(
+        sys.stdout,
+        colorize=True,
+        format="<green>{time} {level} {message}</green>",
+        backtrace=True,
+        diagnose=True,
+        enqueue=True,
+    )
 
 logging.getLogger("werkzeug").disabled = True
--- worker.py
+++ worker.py
@@ -5,5 +5,4 @@ from logger import logger
 def count_words_at_url(url):
     """counts word in url website"""
     resp = requests.get(url)
-    logger.log("WORKER", "A worker completed its job!")
     return len(resp.text.split())
